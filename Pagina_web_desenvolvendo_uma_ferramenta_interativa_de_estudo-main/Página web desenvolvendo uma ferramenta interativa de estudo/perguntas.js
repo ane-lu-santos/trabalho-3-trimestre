35e834a8eca64c0f83050fe1d30354dcc828682f
ane-lu-santos <ane.bonfim.santos@escola.pr.gov.br>
2024-09-24 07:57:53 -0300

    Add files via upload

---
+++ Pagina_web_desenvolvendo_uma_ferramenta_interativa_de_estudo-main/Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,46 @@
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é Python?', //pergunta
+    'Foi criada em 29 de outubro de 1969.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'São cabos de fibra óptica, que são projetados para transmitir dados a distãncias submarinas gigantescas. Enviam e recebem informações em questão de milissegundos. ' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'É um dispositivo que fornece wi-fi. Ele envia informações da internet a dispositivos pessoais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é Python?', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
