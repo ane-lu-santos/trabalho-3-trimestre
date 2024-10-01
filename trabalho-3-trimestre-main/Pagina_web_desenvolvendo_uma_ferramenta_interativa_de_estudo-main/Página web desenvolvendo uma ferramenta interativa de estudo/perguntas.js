9ad86bab1f87dc55ec74ae25986babd0f780c27b
ane-lu-santos <ane.bonfim.santos@escola.pr.gov.br>
2024-10-01 07:55:37 -0300

    Add files via upload

---
+++ trabalho-3-trimestre-main/Pagina_web_desenvolvendo_uma_ferramenta_interativa_de_estudo-main/Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,65 @@
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    'Foi criada em 29 de outubro de 1969.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'O que são cabos de transmissão intercontinentais de internet?', //pergunta
+    'São cabos de fibra óptica, que são projetados para transmitir dados a distãncias submarinas gigantescas. Enviam e recebem informações em questão de milissegundos. ' //resposta
+)
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    ' Qual a sua velocidade de transmissão dos cabos de transmissão intercontinentais?', //pergunta
+    'Proteger os direitos fundamentais de liberdade e de privacidade e a livre formação da personalidade de cada indivíduo.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que são roteadores?', //pergunta
+    'É um dispositivo que fornece wi-fi. Ele envia informações da internet a dispositivos pessoais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'O que são servidores?', //pergunta
+    'São computadores ou sistemas computacionais que atendem requesições de dispositivos clientes, através de uma ou mais redes.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Programação', //tópico da perguntaoteger os direitos fundamentais de liberdade e de privacidade e a livre formação da personalidade de cada indivíduo
+    'O que são cibercrimes?', //pergunta
+    'Crime digital, abrange as atividades criminosas que acontecem ou se iniciam em ambientes vituais, por meio das tecnologias.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Cite exemplos de cibercrimes?', //pergunta
+    'Roubar dados confidenciais, usar o computador para realizar outros atos criminosos ou causar danos aos dados.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é a LGPD?', //pergunta
+    'Estabelece um conjunto de regras para coleta, tratamento, armazenamento e compartilhamento de dados pessoais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Qual é o objetivo da LGPD?', //pergunta
+    'Proteger os direitos fundamentais de liberdade e de privacidade e a livre formação da personalidade de cada indivíduo.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'O que é inteligência artificial?', //pergunta
+    'Conjuntos de tecnologias que permitem aos computadores executar uma variedades de funções avançadas.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Qual foi a primeira inteligência artigicial a se criada? Quando e por quem?', //pergunta
+    'A máquina de Turing, foi criada em 1936 por Alan Mathison Turing.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Qual o impacto da integência artificial, no mundo atual?', //pergunta
+    'Educação:Entrada de muitas plataformas em quase todas as matérias.' //resposta
+)
